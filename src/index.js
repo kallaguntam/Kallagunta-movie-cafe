607a73b55aa087c136dc78a7f2e66240b7ce9950
Mahesh Kallagunta <subbu@Maheshs-MacBook-Air.local>
2020-07-04 11:44:58 +0530

    index file change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import './app.css';
+import './App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
